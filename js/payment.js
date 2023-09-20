71ac2df5c9352cbd1358b85099021814396e111d
Alina <113042968+AlinaKorotkova@users.noreply.github.com>
2023-09-20 21:55:29 +0300

    Update payment.js

--- js/payment.js
+++ js/payment.js
@@ -1,29 +1,33 @@
-let selectSeanse = JSON.parse(sessionStorage.selectSeanse);
+const selectSeanse = JSON.parse(localStorage.selectSeanse);
+
 let places = "";
 let price = 0;
 
-selectSeanse.salesPlaces.forEach(salePlace => {
-	if (places) {
+for (const {
+		row, place, type
+	}
+	of selectSeanse.salesPlaces) {
+	if (places !== "") {
 		places += ", ";
-	};
-	places += `${salePlace.row}/${salePlace.place}`;
-	price += salePlace.type === "standart" ? Number(selectSeanse.priceStandart) : Number(selectSeanse.priceVip);
-});
+	}
+	places += `${row}/${place}`;
+	price += type === "standart" ? Number(selectSeanse.priceStandart) : Number(selectSeanse.priceVip);
+}
 
-document.querySelector(".ticket__title").innerHTML = selectSeanse.filmName;
-document.querySelector(".ticket__chairs").innerHTML = places;
-document.querySelector(".ticket__hall").innerHTML = selectSeanse.hallName;
-document.querySelector(".ticket__start").innerHTML = selectSeanse.seanceTime;
-document.querySelector(".ticket__cost").innerHTML = price;
+document.querySelector(".ticket__title").innerHTML = selectSeanse.filmName;  
+document.querySelector(".ticket__chairs").innerHTML = places; 
+document.querySelector(".ticket__hall").innerHTML = selectSeanse.hallName;  
+document.querySelector(".ticket__start").innerHTML = selectSeanse.seanceTime;  
+document.querySelector(".ticket__cost").innerHTML = price;  
 
-let newHallConfig = selectSeanse.hallConfig.replace(/selected/g, "taken");
-console.log(newHallConfig);
-
-document.getElementById("acceptin-button").addEventListener("click", (event) => {
+const newHallConfig = selectSeanse.hallConfig.replace(/selected/g, "taken");
+document.querySelector(".acceptin-button").addEventListener("click", (event) => {
 	event.preventDefault();
-	let request = `event=sale_add&timestamp=${selectSeanse.seanceTimeStamp}&hallId=${selectSeanse.hallId}&seanceId=${selectSeanse.seanceId}&hallConfiguration=${newHallConfig}`;
-	
-	sendRequest(request, ()=> {
-		window.location.href='ticket.html';
+	fetch("https://jscp-diplom.netoserver.ru/", {
+		method: "POST",
+		headers: {
+			'Content-Type' : 'application/x-www-form-urlencoded'
+		},
+		body: `event=sale_add&timestamp=${selectSeanse.seanceTimeStamp}&hallId=${selectSeanse.hallId}&seanceId=${selectSeanse.seanceId}&hallConfiguration=${newHallConfig}`,
 	});
 });
